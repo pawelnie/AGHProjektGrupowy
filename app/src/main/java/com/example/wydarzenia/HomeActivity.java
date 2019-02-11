4d6120f841abd85cef69fa7a5d9c80e33d34cdf2
PLPANIE <pawel.niesciur@gmail.com>
2019-02-11 16:35:39 +0100

    recyclerView layout

--- app/src/main/java/com/example/wydarzenia/HomeActivity.java
+++ app/src/main/java/com/example/wydarzenia/HomeActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
@@ -24,6 +26,17 @@ public class HomeActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        initializeContent();
+    }
+
+    private void initializeContent() {
+        final RecyclerView recyclerEvents = (RecyclerView) findViewById(R.id.list_events);
+        final LinearLayoutManager eventsLayoutManager = new LinearLayoutManager(this);
+        recyclerEvents.setLayoutManager(eventsLayoutManager);
+
+
+
     }
 
 }
