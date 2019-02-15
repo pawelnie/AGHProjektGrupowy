73bcfd2eb7c8cbc23f075519e2596f0bf41f6d3d
PLPANIE <pawel.niesciur@gmail.com>
2019-02-15 16:47:12 +0100

    Simple RecyclerView working - few warning fixes

--- app/src/main/java/com/example/wydarzenia/Dane.java
+++ app/src/main/java/com/example/wydarzenia/Dane.java
@@ -3,12 +3,12 @@ package com.example.wydarzenia;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Dane {
+class Dane {
     private static Dane ourInstance = null;
 
-    List<EventInfo> mEvents = new ArrayList<>();
+    private List<EventInfo> mEvents = new ArrayList<>();
 
-    public static Dane getInstance() {
+    static Dane getInstance() {
         if(ourInstance == null) {
             ourInstance = new Dane();
             ourInstance.initializeEvents();
@@ -28,7 +28,7 @@ public class Dane {
         mEvents.add(6, new EventInfo("Event2"));
 
     }
-    public List<EventInfo> getEvents() {
+    List<EventInfo> getEvents() {
         return mEvents;
     }
 
--- app/src/main/java/com/example/wydarzenia/EventInfo.java
+++ app/src/main/java/com/example/wydarzenia/EventInfo.java
@@ -3,16 +3,16 @@ package com.example.wydarzenia;
 import java.util.ArrayList;
 import java.util.List;
 
-public class EventInfo {
+class EventInfo {
     private String mTitle;
 
 
-    public EventInfo(String title) {
+    EventInfo(String title) {
         mTitle = title;
     }
 
 
-    public String getTitle() {
+    String getTitle() {
         return mTitle;
     }
 }
